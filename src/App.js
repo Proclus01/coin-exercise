d7e030eae20dc20ff0f7faa52acbe36af7904805
Neven <95269427+Proclus01@users.noreply.github.com>
2022-09-04 22:34:48 -0700

    make empty boilerplate for components and wire up into app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Tracker from './Tracker.js';
 
 function App() {
   return (
     <div className="App">
-      Coin Tracker!
+      <Tracker />
     </div>
   );
 }
---
+++ src/Tracker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Coin from './Coin.js';
+
+class Tracker extends Component {
+    render() {
+        return (
+            <div>
+                <Coin />
+            </div>
+        );
+    }
+}
+
+export default Tracker;
